77d704b298bf9a7fafd927372a30854b168565f7
rafaelgcaldas01 <rafael.gcaldas01@gmail.com>
2021-06-24 23:04:55 -0300

    Check answered and highlighted

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -47,7 +47,7 @@ export function useRoom(roomId: string) {
           id: key,
           content: value.content,
           author: value.author,
-          isHighLighted: value.isHighlighted,
+          isHighlighted: value.isHighlighted,
           isAnswered: value.isAnswered,
           likeCount: Object.values(value.likes ?? {}).length,
           likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
